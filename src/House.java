af2f74b88c1d2e16755c90fd5f9b37f1a1163c36
ebradford <emma_bradford@milton.edu>
2026-01-16 16:55:07 -0500

    Initial commit

---
+++ src/House.java
@@ -0,0 +1,4 @@
+public class House {
+    public String message = "A7#q 9mA$%2mn!!8my@4mt^6mh&1mi*3mn(0mgZ9m 4wfymt@7mh# 5ma$2mt!8m mmma%3mk^4me&6ms*1m Q12mm &2-me#7m mc$9mu!5mt@8m 6hM.mt^4mh&6me *3m R7mc@2mh#9me$5me!832ms^4me&6m.";
+    
+}
